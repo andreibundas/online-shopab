ae58165a146f2faea23a93af85709c15f24b7721
Andrei Bundas <andrei.bundas@gmail.com>
2021-07-05 23:11:46 +0300

    Initial commit (project structure)

---
+++ src/main/java/org/fasttrackit/onlineshopab/OnlineShopabApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.onlineshopab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopabApplication.class, args);
+	}
+
+}
